ccecca09a28facbb0163eedf5a16f6e7dc1d7d0e
SAAD MOURTADA <saad.mourtada@gmail.com>
2018-12-18 14:09:31 -0600

    Updates

---
+++ precache-manifest.b2eb7758389cb1dffaebe642811df518.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a79274d3d3ca0aceedb2",
+    "url": "/Track-Your-Money/static/js/runtime~main.a79274d3.js"
+  },
+  {
+    "revision": "9ac49c96d7f3309dc794",
+    "url": "/Track-Your-Money/static/js/main.9ac49c96.chunk.js"
+  },
+  {
+    "revision": "31e5f89fc659c51e14a6",
+    "url": "/Track-Your-Money/static/js/1.31e5f89f.chunk.js"
+  },
+  {
+    "revision": "9ac49c96d7f3309dc794",
+    "url": "/Track-Your-Money/static/css/main.c2ee9dd7.chunk.css"
+  },
+  {
+    "revision": "b0a4e8ef745adee7d37a93e316c1449c",
+    "url": "/Track-Your-Money/index.html"
+  }
+];
